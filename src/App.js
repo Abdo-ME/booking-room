ca84e420662576b832c7343419d78af0bd3aead6
Abdo-ME <abdelhakmehda@gmail.com>
2022-06-21 18:26:41 +0100

    initial App

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Home from './pages/home/Home'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
